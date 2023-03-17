ae5baa917860c9d15ad2566669600bbfa0c192ff
jesuschapulin <chaps.jesus.gz@gmail.com>
2023-03-16 22:51:34 -0600

    se suben ultimas modificaciones para buscar patrones de email

--- index.js
+++ index.js
@@ -4,12 +4,98 @@ console.log("Hello, World! web scraping")
 const cheerio = require("cheerio")
 const axios = require("axios")
 const arrayData = [];
+const arrayDataLatindex = [];
+const arrayDataCSI = [];
 const arrayDataSaved = [];
+const arrayDataSavedRelations = [];
 const cadenaEmails = [];
 var cadenaSimple = "";
 var fileUrl = "./srcData/journalcsvIT10.csv";
 var fileUrlSaved = "./srcData/journalDataFinish.csv";
+var fileLatindexUrl = "./srcData/latindex2023T10.csv";
+var fileCsimagoUrl = "./srcData/scimagojr 2021T10.csv";
+var fileRelationsLatUrlSaved = "./srcData/journalDataFinish-relationLatindex.csv";
 var emailFound = "";
+async function readRelationsLatindexCsi() {
+    let dataFull;
+    const fs = require("fs");
+
+    function setTowrite(data) {
+        if (data !== "" && data.split(",").length > 0) {
+            fs.writeFile(fileRelationsLatUrlSaved, data, "utf-8", (err) => {
+                if (err) console.log(err);
+                else console.log("-------------Data saved-----------------");
+            });
+        } else {
+            console.log("-------------Data  not saved-----------------");
+            console.log(data);
+        }
+    }
+
+    function readInfoFromFileSaved() {
+        fs.readFile(fileRelationsLatUrlSaved, "utf-8", (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            const lines = data.split(/\r\n|\r|\n/, -1);
+            console.log("------------tamaño de lineas de csv saved----------" + lines.length);
+            cont = 0;
+            lines.forEach((item, value) => {
+                cont++;
+                if (item != "") {
+                    arrayDataSavedRelations.push(item);
+                    console.log("-------------leido del csv guardado------------------------");
+                    console.log(item);
+                }
+            });
+            console.log("---------------data full saved-------------");
+            console.log(lines);
+        });
+    }
+
+    function createRelationsJournals(nombre, issn, eissn, correo) {
+        cadenaSimple+=nombre+","+ issn+","+ eissn+","+ correo+"\n";
+        console.log("--------------- reading every foreach-------------");
+        readInfoFromFileSaved();
+        setTowrite(cadenaSimple);
+    }
+    fs.readFile(fileLatindexUrl, "utf-8", (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        //else console.log(data);
+        const lines = data.split(/\r\n|\r|\n/, -1);
+        //console.log( lines ); // HTML content of the jQuery.ajax page
+        console.log("tamaño de lineas de csv:::::" + lines.length);
+        cont = 0;
+        lines.forEach((item, value) => {
+            cont++;
+            var nombre = item.split(",").length > 0 ? (item.split(",")[1]) : nombre;
+            var issn = (item.split(",").length > 0 && item.split(",")[2].split("-").length > 0) ? (item.split(",")[2]) : issn;
+            issn = (item.split(",").length > 0 && item.split(",")[3].split("-").length > 0 && issn=="") ? (item.split(",")[3]) : issn;
+            var Eissn = (item.split(",").length > 4 && item.split(",")[4].split("-").length > 0) ? (item.split(",")[4]) : "";
+            Eissn = (item.split(",").length > 5 && item.split(",")[5].split("-").length > 0 && Eissn=="") ? (item.split(",")[5]) : Eissn;
+            var correo = (item.split(",").length >= 3 && item.split(",")[3]!="") ? (item.split(",")[3]) : "";
+            correo = (item.split(",").length > 7 && item.split(",")[7] && correo=="") ? (item.split(",")[7]) : correo;
+            console.log("tam de linea en ,"+item.split(",").length+" :::::: item :::: " + item);
+            console.log(":::::: issn :::: " + issn + "::::::: eissn::::: " + Eissn);
+            const objfromcsv = {
+                nombre: nombre,
+                issn: issn,
+                Eissn: Eissn,
+                correo: correo
+            }
+                arrayDataLatindex.push(objfromcsv);
+                console.log("::::::nombre:::: " + nombre + "\n:::::::correo::::: " + correo);
+            
+        });
+        console.log("tamaño de lineas de csv minified:::::" + arrayDataLatindex.length);
+        arrayDataLatindex.forEach((item, value) => {
+            console.log("::::::::::::: leyendo obj de csv ::::::::::::::::: ");
+            setTimeout(createRelationsJournals, 1000, item.nombre, item.issn, item.Eissn, item.correo);
+        });
+    }); ////finish of read csv
+}
 async function performScraping() {
     let dataFull;
     const fs = require("fs");
@@ -64,7 +150,13 @@ async function performScraping() {
             hsrIdentifier: 'HSR',
             page: 7
         }
-        axios.get(url).then(res => {
+        const options = {
+            xsrfCookieName: 'XSRF-TOKEN',
+            xsrfHeaderName: 'X-XSRF-TOKEN',
+        };
+        axios.get(url, {}, {
+            timeout: 6000
+        }).then(res => {
             const $ = cheerio.load(res.data)
             // parsing the HTML source of the target web page with Cheerio
             //const fileData = cheerio.load(fileResponse.data)
@@ -208,8 +300,7 @@ async function performScraping() {
                         }
                     }
                 });
-            }
-            else if ($("#contact").html() !== null) {
+            } else if ($("#contact").html() !== null) {
                 console.log("#contact>>>>>>>>>>>>>");
                 $("#contact").find("div").each((index, element) => {
                     const email = $(element).find("p").text();
@@ -218,7 +309,7 @@ async function performScraping() {
                     if (email != "") {
                         const lines = email.split("\n");
                         var justEmail = "";
-                        console.log("sizelines of page element>>>>>>>>>>>>>"+ lines.length);
+                        console.log("sizelines of page element>>>>>>>>>>>>>" + lines.length);
                         for (var is = 0; is < lines.length; is++) {
                             console.log("checando>>>>>>>>>>>>>>>>>>>>>>>>")
                             console.log(lines[is])
@@ -240,7 +331,7 @@ async function performScraping() {
                                         urlJournalIssn: urlJournalIssn,
                                         urlJournalEissn: urlJournalEissn
                                     }
-                                    console.log("seteando email de pagina>>>>>>>>>>>>>"+ justEmail);
+                                    console.log("seteando email de pagina>>>>>>>>>>>>>" + justEmail);
                                     emailLatest = justEmail;
                                     emailFound = "OK";
                                     cadenaSimple += nombre + "," + url + "," + "Found:" + emailFound + "," + justEmail + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
@@ -248,8 +339,104 @@ async function performScraping() {
                                 }
                             }
                         }
-                        if (justEmail=="" && added == "") {
-                            console.log(">>>>>>>>>>>seteando elemento vacio>>>>>>>>>>>>>"+ justEmail);
+                        console.log(">>>>>>>>>>> positions >>>>>>>>>>>>>" + lines.length + " >>>>>>>> " + is);
+                        if (lines.length - 1 == is && emailLatest == "" && added == "") {
+                            console.log(">>>>>>>>>>>seteando elemento vacio>>>>>>>>>>>>>" + justEmail);
+                            emailFound = "";
+                            added = "seted";
+                            cadenaSimple += nombre + "," + url + "," + "Found: NOT!!!" + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
+                        }
+                    }
+                });
+            }else if ($(".contact").html() !== null) {
+                console.log(".contact;;;;;;;;;;;;;");
+                $(".contact").find(".email").each((index, element) => {
+                    const email = $(element).find("a").text();
+                    console.log("data div email;;;;;;;;;;;;;");
+                    //console.log($(element).html())
+                    if (email != "") {
+                        const lines = email.split("\n");
+                        var justEmail = "";
+                        console.log("sizelines of page element;;;;;;;;;;;;;" + lines.length);
+                        for (var is = 0; is < lines.length; is++) {
+                            console.log("checando;;;;;;;;;;;;;;;;;;;;;;;;")
+                            console.log(lines[is])
+                            if (lines[is].includes("\@")) {
+                                //console.log("lineEmail;;;;;;;;;;;;;"+ lines[is].trim());
+                                if (lines[is].includes(":")) {
+                                    justEmail = "" + lines[is].split(":")[1].trim();
+                                    added = "seted";
+                                } else {
+                                    justEmail = "" + lines[is].trim();
+                                    added = "seted";
+                                }
+                                if (justEmail != "" && justEmail != emailLatest) {
+                                    const journalEmail = {
+                                        nombre: nombre,
+                                        email: justEmail,
+                                        urlJournal: url,
+                                        urlJournalDoaj: urlJournalDoaj,
+                                        urlJournalIssn: urlJournalIssn,
+                                        urlJournalEissn: urlJournalEissn
+                                    }
+                                    console.log("seteando email de pagina;;;;;;;;;;;;;" + justEmail);
+                                    emailLatest = justEmail;
+                                    emailFound = "OK";
+                                    cadenaSimple += nombre + "," + url + "," + "Found:" + emailFound + "," + justEmail + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
+                                    added = "seted";
+                                }
+                            }
+                        }
+                        console.log(";;;;;;;;;;; positions ;;;;;;;;;;;;;" + lines.length + " ;;;;;;;; " + is);
+                        if (lines.length - 1 == is && emailLatest == "" && added == "") {
+                            console.log(";;;;;;;;;;;seteando elemento vacio;;;;;;;;;;;;;" + justEmail);
+                            emailFound = "";
+                            added = "seted";
+                            cadenaSimple += nombre + "," + url + "," + "Found: NOT!!!" + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
+                        }
+                    }
+                });
+            } else if ($(".footer").html() !== null) {
+                console.log(".footer-------------");
+                $(".footer").each((index, element) => {
+                    const email = $(element).find("a").text();
+                    console.log("data div email-------------");
+                    //console.log($(element).html())
+                    if (email != "") {
+                        const lines = email.split("\n");
+                        var justEmail = "";
+                        console.log("sizelines of page element-------------" + lines.length);
+                        for (var is = 0; is < lines.length; is++) {
+                            console.log("checando------------------------")
+                            console.log(lines[is])
+                            if (lines[is].includes("\@")) {
+                                //console.log("lineEmail-------------"+ lines[is].trim());
+                                if (lines[is].includes(":")) {
+                                    justEmail = "" + lines[is].split(":")[1].trim();
+                                    added = "seted";
+                                } else {
+                                    justEmail = "" + lines[is].trim();
+                                    added = "seted";
+                                }
+                                if (justEmail != "" && justEmail != emailLatest) {
+                                    const journalEmail = {
+                                        nombre: nombre,
+                                        email: justEmail,
+                                        urlJournal: url,
+                                        urlJournalDoaj: urlJournalDoaj,
+                                        urlJournalIssn: urlJournalIssn,
+                                        urlJournalEissn: urlJournalEissn
+                                    }
+                                    console.log("seteando email de pagina-------------" + justEmail);
+                                    emailLatest = justEmail;
+                                    emailFound = "OK";
+                                    cadenaSimple += nombre + "," + url + "," + "Found:" + emailFound + "," + justEmail + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
+                                    added = "seted";
+                                }
+                            }
+                        }
+                        if (justEmail == "" && added == "") {
+                            console.log("-----------seteando elemento vacio-------------" + justEmail);
                             emailFound = "";
                             added = "seted";
                             cadenaSimple += nombre + "," + url + "," + "Found: NOT!!!" + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
@@ -257,7 +444,6 @@ async function performScraping() {
                     }
                 });
             }
-            
             if (emailLatest == "" && added == "") {
                 console.log("+++++++++++seteando elemento vacio+++++++++++++");
                 added = "seted";
@@ -281,7 +467,6 @@ async function performScraping() {
             console.log("+++++++++++seteando elemento vacio+++++++++++++");
             added = "seted";
             cadenaSimple += nombre + "," + url + "," + "Found: NOT!!!" + "," + urlJournalIssn + "," + urlJournalEissn + "\n";
-            
             readInfoFromFileSaved();
             setTowrite(cadenaSimple);
         }); ///fin axios get
@@ -299,25 +484,28 @@ async function performScraping() {
         cont = 0;
         lines.forEach((item, value) => {
             cont++;
+            var nombre = item.split(",http").length > 0 ? (item.split(",http")[0]) : nombre;
+            var urlJournal = item.split(",http").length > 0 ? "http" + (item.split(",http")[1]) : urlJournal;
+            urlJournal = urlJournal.includes(",") ? urlJournal.split(",")[0] : urlJournal;
             const objfromcsv = {
-                nombre: item.split(",")[0],
-                urlJournal: item.split(",")[1],
+                nombre: nombre,
+                urlJournal: urlJournal,
                 urlJournalDoaj: item.split(",")[2],
                 urlJournalIssn: item.split(",")[5],
                 urlJournalEissn: item.split(",")[6],
             }
             if (value != "" && cont > 1) {
                 arrayData.push(objfromcsv);
-                /*           console.log(item.split(",")[1]);
-                 */
+                console.log(item.split(",")[1]);
+                console.log("::::::nombre:::: " + nombre + "\n:::::::url::::: " + urlJournal);
             }
         });
         console.log("tamaño de lineas de csv minified:::::" + arrayData.length);
         arrayData.forEach((item, value) => {
-            var url = (~item.urlJournal.indexOf(".php/") && !item.urlJournal.endsWith("/index")) ? item.urlJournal + "/about/contact" : item.urlJournal;
+            var url = (~item.urlJournal.indexOf(".php/") && !item.urlJournal.endsWith("/index") && !item.urlJournal.includes("www.scielo.")) ? item.urlJournal + "/about/contact" : item.urlJournal;
             url = item.urlJournal.endsWith("/index") ? item.urlJournal.substring(0, item.urlJournal.length - 5) + "about/contact" : url;
             url = item.urlJournal.endsWith("home/afr") ? item.urlJournal.substring(0, item.urlJournal.length - 8) + "description/AFR" : url;
-            console.log(":::::::::::::url ::::::::::::::::: " + url);
+            console.log(":::::::::::::url formada ::::::::::::::::: " + url);
             for (var sec = 0; sec < 3; sec++) {
                 console.log("estado de email::::::::::::::::: ");
                 console.log("email found::::::::: " + emailFound);
@@ -325,7 +513,7 @@ async function performScraping() {
                 console.log("***********peticion a pagina de revista*******");
                 console.log("url leida:::::::::::::::::" + "poss::::" + sec + "  " + url);
                 if (emailFound == "" && sec == 0) {
-                    getEmailPage(url, item.nombre, item.urlJournalDoaj, item.urlJournalIssn, item.urlJournalEissn);
+                    setTimeout(getEmailPage, 2000, url, item.nombre, item.urlJournalDoaj, item.urlJournalIssn, item.urlJournalEissn);
                 } else if (emailFound != "OK" && sec == 4) {
                     console.log("***********no se encontro el email en su pagina se intenta con doaj*******");
                     console.log("url leida segunda::::::::::::::::: " + item.urlJournalDoaj);
@@ -337,3 +525,4 @@ async function performScraping() {
     }); ////finish of read csv
 }
 performScraping();
+/* readRelationsLatindexCsi(); */
